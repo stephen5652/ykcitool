28fa8798f1d7694c56b32ead07929e4933558e8a
stephen.chen <stephen@StephenChen-MacBookPro.local>
2022-09-21 11:41:12 +0800

    chore: update version to 0.2.13

--- lib/ykfastlane/version.rb
+++ lib/ykfastlane/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module YKFastlane
-  VERSION = "0.2.12"
+  VERSION = "0.2.13"
   YKFASTLANE_ENV_PATH = File.expand_path(File.join(Dir.home, '.ykfastlane_config'))
 end
